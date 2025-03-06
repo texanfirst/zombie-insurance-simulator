a67de066bf988a1d6ab7eabda2b98e98097eb2f8
texanfirst <79604941+texanfirst@users.noreply.github.com>
2025-03-06 12:36:35 -0600

    Fix ESLint config and add Phaser dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc/dist/eslintrc.cjs';
+import js from '@eslint/js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  js.configs.recommended,
+  ...compat.extends('next/core-web-vitals'),
 ];
-
-export default eslintConfig;
